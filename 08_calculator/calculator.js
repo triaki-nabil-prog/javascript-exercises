2c315e2e10736af0ad8a48f5bfc0aabcf0261ea8
triaki-nabil-prog <nabil.triaki2@gmail.com>
2022-10-20 23:32:45 +0100

    javascript calculator exercise from the odin site

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,27 @@
-const add = function() {
-	
+const add = function (a, b) {
+  return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function (a, b) {
+  return a - b;
 };
 
-const sum = function() {
-	
+const sum = function (Numbers) {
+  return Numbers.reduce((sum, Number) => (sum += Number), 0);
 };
 
-const multiply = function() {
-
+const multiply = function (Numbers) {
+  return Numbers.reduce((mul, Number) => (mul *= Number), 1);
 };
 
-const power = function() {
-	
+const power = function (a, b) {
+  return Math.pow(a, b);
 };
 
-const factorial = function() {
-	
+const factorial = function (Number) {
+  let factorial = 1;
+  for (let i = Number ; i > 0; i--) { factorial *= i; }
+  return factorial;
 };
 
 // Do not edit below this line
