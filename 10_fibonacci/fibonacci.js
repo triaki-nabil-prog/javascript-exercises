4ab20ffc27078330753a9cadde067ecdfb0b7969
triaki-nabil-prog <nabil.triaki2@gmail.com>
2022-10-21 12:10:26 +0100

    fibonnaci number

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,16 @@
-const fibonacci = function() {
+const fibonacci = function (number) {
+    if(number <0) return "OOPS";
+    if (number === 0) return 0;
 
+    let a = 0;
+    let b = 1;
+
+    for (let i = 1; i < number; i++) {
+        const temp = b;
+        b = a + b;
+        a = temp;
+    }
+    return b;
 };
 
 // Do not edit below this line
