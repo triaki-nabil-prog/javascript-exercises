0c6b5f26db1cc106b31d389ff560f74341f4bddd
triaki-nabil-prog <nabil.triaki2@gmail.com>
2022-10-22 01:43:12 +0100

    javaScript exercise that will let me practice how to find values in array objects and usage of reduce

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,20 @@
-const findTheOldest = function() {
+const findTheOldest = function(objArray) {
+return objArray.reduce((oldest, currentPerson) => {
+const currentPersonAge = getAge(currentPerson.yearOfDeath, currentPerson.yearOfBirth);
+const oldestAge = getAge(oldest.yearOfDeath, oldest.yearOfBirth);
+return oldestAge >=currentPersonAge ? oldest : currentPerson ;
+});
 
 };
+
+
+function getAge(death, birth){
+if ( !death ) {
+    death =new Date().getFullYear();
+}
+return death - birth;
+
+}
 
 // Do not edit below this line
 module.exports = findTheOldest;
